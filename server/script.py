6a65df970f3c5936c36d5903f1962d0fc6479b95
rogerliu37 <rliueml@gmail.com>
2021-11-06 12:05:52 -0400

    nit

--- server/script.py
+++ server/script.py
@@ -4,6 +4,7 @@ import os
 os.environ['SPOTIPY_CLIENT_ID'] = 'c97c4d2a24b5431a9decec1112f2139d'
 os.environ['SPOTIPY_CLIENT_SECRET'] = '4d1690c7b5764bc48ca7dab4d46b2fbf'
 
+# iu uri
 artist_uri = 'spotify:artist:3HqSLMAZ3g3d5poNaI7GOU'
 spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
 
