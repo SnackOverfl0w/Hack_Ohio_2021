fa63729108366aa52df6375f52b4d49abb573381
rogerliu37 <rliueml@gmail.com>
2021-11-06 11:53:08 -0400

    find iu albums

--- server/script.py
+++ server/script.py
@@ -1,1 +1,17 @@
-print('helloworld')
+import spotipy
+from spotipy.oauth2 import SpotifyClientCredentials
+import os
+os.environ['SPOTIPY_CLIENT_ID'] = 'c97c4d2a24b5431a9decec1112f2139d'
+os.environ['SPOTIPY_CLIENT_SECRET'] = '4d1690c7b5764bc48ca7dab4d46b2fbf'
+
+artist_uri = 'spotify:artist:3HqSLMAZ3g3d5poNaI7GOU'
+spotify = spotipy.Spotify(client_credentials_manager=SpotifyClientCredentials())
+
+results = spotify.artist_albums(artist_uri, album_type='album')
+albums = results['items']
+while results['next']:
+    results = spotify.next(results)
+    albums.extend(results['items'])
+
+for album in albums:
+    print(album['name'])
